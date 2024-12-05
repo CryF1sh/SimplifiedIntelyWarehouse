1e597f8b56dc986eec40567857b978af736cdf03
Глеб <glebbogomolov04@gmail.com>
2024-12-05 14:41:38 +0300

    add db context

--- SimplifiedIntelyWarehouse/Infrastructure/Data/SIWDbContext.cs
+++ SimplifiedIntelyWarehouse/Infrastructure/Data/SIWDbContext.cs
@@ -1,6 +1,22 @@
-﻿namespace SimplifiedIntelyWarehouse.Infrastructure.Data
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SimplifiedIntelyWarehouse.Infrastructure.Data
 {
-    public class SIWDbContext
+    public class SIWDbContext : DbContext
     {
+        //public DbSet<Product> Products { get; set; }
+        //public DbSet<Stock> Stocks { get; set; }
+        //public DbSet<Transaction> Transactions { get; set; }
+
+        public SIWDbContext(DbContextOptions<SIWDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Настройка сущностей
+        }
     }
 }
