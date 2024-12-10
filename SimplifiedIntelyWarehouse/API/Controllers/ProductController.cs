2bd63527ac9e7896168405dc4ea04ecbceca66eb
Глеб <glebbogomolov04@gmail.com>
2024-12-11 00:10:44 +0300

    add post method in productcontroller

--- SimplifiedIntelyWarehouse/API/Controllers/ProductController.cs
+++ SimplifiedIntelyWarehouse/API/Controllers/ProductController.cs
@@ -11,7 +11,7 @@ namespace SimplifiedIntelyWarehouse.API.Controllers
     {
         private readonly SIWDbContext _context;
 
-        public ProductController (SIWDbContext context)
+        public ProductController(SIWDbContext context)
         {
             _context = context;
         }
@@ -22,5 +22,52 @@ namespace SimplifiedIntelyWarehouse.API.Controllers
         {
             return await _context.Products.ToListAsync();
         }
+
+
+        // GET: api/Product/{id}
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProductById(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+
+            if (product == null) return NotFound();
+
+            return product;
+        }
+
+        // GET: api/Product/{name}
+        [HttpGet("{name}")]
+        public async Task<ActionResult<Product>> GetProductByName(string name)
+        {
+            var product = await _context.Products.FindAsync(name);
+
+            if (product == null) return NotFound();
+
+            return product;
+        }
+
+        // GET: api/Product/{sku}
+        [HttpGet("{sku}")]
+        public async Task<ActionResult<Product>> GetProductBySKU(string sku)
+        {
+            var product = await _context.Products.FindAsync(sku);
+
+            if (product == null) return NotFound();
+
+            return product;
+        }
+
+        // POST: api/Product/create
+        [HttpPost]
+        public async Task<ActionResult<Product>> CreateProduct([FromBody] Product product)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetProductById), new { id = product.Id }, product);
+        }
+
+
     }
 }
