6cf5de0f78cb048402058a5364b2da46c603195d
Глеб <glebbogomolov04@gmail.com>
2024-12-10 23:49:11 +0300

    add v1 product controller

---
+++ SimplifiedIntelyWarehouse/API/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SimplifiedIntelyWarehouse.Core.Entities;
+using SimplifiedIntelyWarehouse.Infrastructure.Data;
+
+namespace SimplifiedIntelyWarehouse.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly SIWDbContext _context;
+
+        public ProductController (SIWDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Product
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            return await _context.Products.ToListAsync();
+        }
+    }
+}
